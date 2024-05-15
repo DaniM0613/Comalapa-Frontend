0772612893e1ecb1a9ff9ccdbf9393738c6fd0ea
GlendyT <glendytuyuc@gmail.com>
2024-05-15 15:09:22 -0600

    end responsive stuff

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -20,21 +20,21 @@ const Cart = () => {
           <p>Remove</p>
         </div>
         <br/>
-        <hr/>
+        <hr className="bg-gray-600 h-0.5 border-none" />
         {menu_list.map((item,index) => {
            if(cartItems[item._id]>0) 
            {
              return (
-              <div>  
-                 <div className="">
-                    <img src={item.image} alt=""/>
-                       <p>{item.name}</p>
-                       <p>${item.price}</p>
+              <div key={index}>  
+                 <div  className="grid grid-cols-6 items-center text-center text-gray-500 text-sm mx-0 my-2 max-sm:gap-3 max-sm:text-center">
+                    <img src={item.image} alt="" className="w-12"/>
+                       <p className="max-sm:text-xs">{item.name}</p>
+                       <p>Q{item.price}</p>
                        <p>{cartItems[item._id]}</p>
-                       <p>${item.price*cartItems[item._id]}</p>
-                       <p onClick={() =>removeFromCart(item._id)} className="">X</p>
+                       <p>Q{item.price*cartItems[item._id]}</p>
+                       <p onClick={() =>removeFromCart(item._id)} className="cursor-pointer bg-red-600 rounded-2xl mx-14 max-lg:mx-8 text-white font-bold text-center p-0.5 max-sm:mx-2">X</p>
                  </div>
-                <hr/> 
+                <hr className="bg-gray-600 h-0.5 border-none"/> 
               </div>
             );
           }
@@ -42,24 +42,24 @@ const Cart = () => {
       </div>
       <div className="mt-16 flex justify-between gap-32 mb-10 max-sm:flex-col-reverse max-sm:mt-8 max-sm:gap-10">
         <div className="flex-1 flex flex-col gap-5">
-          <h2>Total</h2>
+          <h2 className="font-bold">Total</h2>
           <div>
-            <div className="">
+            <div  className="flex justify-between text-slate-600">
                <p>Subtotal</p>
                <p>Q{getTotalCartAmount()}</p>
              </div>
-             <hr/>
-            <div className=""> 
+             <hr className="my-2.5" />
+            <div  className="flex justify-between text-slate-600"> 
                <p>Delivery Fee</p>
                <p>Q{getTotalCartAmount()===0?0:2}</p>
             </div>
-            <hr/>
-            <div className=""> 
+            <hr className="my-2.5" />
+            <div  className="flex justify-between text-slate-600"> 
                <p>Total</p>
                <b>Q{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button onClick={()=>navigate('/order')}>Procee to checkout</button>
+          <button  className="border-none text-white bg-orange-500 w-44 py-2 rounded-md cursor-pointer uppercase font-bold text-sm" onClick={()=>navigate('/order')}>Procee to checkout</button>
         </div>
         
         <div className="flex-1 max-sm:justify-start">
