cfd708bb1e40434b92d20a074132be393b270f79
DaniM0613 <wwh.rj.92@gmail.com>
2024-05-20 18:21:58 -0600

    Create user authentication (login & registration) feature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react"
 import { assets } from "../../assets/assets"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { StoreContext } from "../../context/StoreContext"
 
 
@@ -8,7 +8,15 @@ const Navbar = ({setShowLogin}) => {
 
     const [ menu, setMenu ] = useState("inicio")
 
-    const {getTotalCartAmount} = useContext(StoreContext);
+    const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
+
+    const navigate = useNavigate();
+
+    const logout = () =>{
+       localStorage.removeItem("token");
+       setToken("");
+       navigate("/")
+    }
 
   return (
     <div className="flex px-0 py-0 justify-between items-center">
@@ -25,7 +33,19 @@ const Navbar = ({setShowLogin}) => {
                <Link  to='/cart'> <img src={assets.basket_icon} alt="" className="w-6 max-sm:w-4"/> </Link> 
                 <div className={getTotalCartAmount()===0?"":"absolute min-w-3 min-h-3 bg-orange-400 rounded-3xl bottom-6 left-4 max-sm:min-h-2 max-sm:min-w-2 max-sm:bottom-3"}></div>
             </div>
+            {!token ?  
             <button onClick={()=>setShowLogin(true)} className=" bg-transparent text-sm text-slate-600 border-2 border-orange-600 py-2 px-2  rounded-3xl cursor-pointer transition-transform hover:bg-red-50 max-sm:text-xs max-sm:w-20 max-sm:py-1 max-sm:px-2">Inicia Sesión</button>
+            : <div className="">
+              <img src={assets.profile_icon} alt=""/>
+              <ul className="">
+                 <li><img src={assets.bag_icon} alt=""/><p>Orders</p> </li>
+                 <hr/>
+                 <li onClick={logout} ><img src={assets.logout_icon} alt="" /><p>Logout</p> </li>
+                 
+              </ul>
+            </div>
+          }
+           
         </div>
       </div>
     
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,6 +7,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000"
+    const [token, setToken] = useState('');
 
     const addToCart = (itemId) => {
        if(!cartItems[itemId]){
@@ -32,6 +34,12 @@ const StoreContextProvider = (props) => {
          }
          return totalAmount;
     }
+
+    useEffect(()=> {
+        if(localStorage.getItem('token')) {
+           setToken(localStorage.getItem('token'))
+        }
+    },[])
     
 
     const contextValue = {
@@ -40,12 +48,12 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
-    useEffect(() => {
-       
-    },cartItems)
      
      return (
         <StoreContext.Provider value={contextValue} >
