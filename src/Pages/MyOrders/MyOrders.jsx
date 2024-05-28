e72f3be5e5af37de335e5130745ab3eb35d447ee
GlendyT <glendytuyuc@gmail.com>
2024-05-27 18:58:33 -0600

    styles to the myorders and the logout

--- src/Pages/MyOrders/MyOrders.jsx
+++ src/Pages/MyOrders/MyOrders.jsx
@@ -3,50 +3,57 @@ import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
 import { assets } from "../../assets/assets";
 
-
 const MyOrders = () => {
- 
-    const {url,token} = useContext(StoreContext);
-    const [data,setData] = useState([]);
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
 
-    const fethOrders  = async () => {
-        const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}});
-        setData(response.data.data);
-        console.log(response.data.data)
+  const fethOrders = async () => {
+    const response = await axios.post(
+      url + "/api/order/userorders",
+      {},
+      { headers: { token } }
+    );
+    setData(response.data.data);
+    console.log(response.data.data);
+  };
+  useEffect(() => {
+    if (token) {
+      fethOrders();
     }
-    useEffect(() => {
-        if(token){
-            fethOrders();
-        }
-    },[token])
+  }, [token]);
 
-    return (
-        <div className="">
-            <h2>My Orders</h2>
-            <div className="">
-                {data.map((order,index)=> {
-                    return (
-                        <div key={index} className="">
-                           <img src={assets.parcel_icon} alt=""/>
-                           <p>{order.items.map((item,index)=> {
-                               if(index=== order.items.length-1) {
-                                  return item.name+" X "+item.quantity
-                                }
-                                else {
-                                  return item.name+" X "+item.quantity+" , "
-                                }
-
-                           })}</p>
-                           <p>${order.amount}.00</p>
-                           <p>Items:{order.items.length} </p>
-                           <p><span>&#x25cf;</span> <b>{order.status}</b></p>
-                           <button onClick={fethOrders}>Track Order</button>
-                        </div>
-                    )
+  return (
+    <div className=" my-12 mx-0 group">
+      <h2>My Orders</h2>
+      <div className="flex flex-col gap-5 mt-7 ">
+        {data.map((order, index) => {
+          return (
+            <div
+              key={index}
+              className="grid grid-cols-6 items-center gap-7 text-xs px-2.5 py-5 text-slate-700 border-solid border border-gray-500 max-sm:grid-cols-3 max-sm:gap-2.5 max-sm:text-xs"
+            >
+              <img src={assets.parcel_icon} alt="" className=" w-12" />
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " X " + item.quantity;
+                  } else {
+                    return item.name + " X " + item.quantity + " , ";
+                  }
                 })}
+              </p>
+              <p>${order.amount}.00</p>
+              <p>Items:{" "}{order.items.length} </p>
+              <p>
+                <span className="text-orange-600">&#x25cf;</span>{" "}
+                <b className=" font-medium text-slate-700 ">{order.status}</b>
+              </p>
+              <button className="border-none px-6 py-0 rounded-md bg-orange-200 cursor-pointer text-slate-800" onClick={fethOrders}>Track Order</button>
             </div>
-
-        </div>
-    )
-}
-export default MyOrders
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default MyOrders;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,54 +1,114 @@
-import { useContext, useState } from "react"
-import { assets } from "../../assets/assets"
-import { Link, useNavigate } from "react-router-dom"
-import { StoreContext } from "../../context/StoreContext"
+import { useContext, useState } from "react";
+import { assets } from "../../assets/assets";
+import { Link, useNavigate } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("inicio");
 
-const Navbar = ({setShowLogin}) => {
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
 
-    const [ menu, setMenu ] = useState("inicio")
+  const navigate = useNavigate();
 
-    const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
-
-    const navigate = useNavigate();
-
-    const logout = () =>{
-       localStorage.removeItem("token");
-       setToken("");
-       navigate("/")
-    }
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  };
 
   return (
     <div className="flex px-0 py-0 justify-between items-center">
-        <Link to='/'> <img src={assets.logo} alt="" className="w-32 max-sm:w-16"/> </Link>
-        <ul className="flex gap-5 list-none text-slate-600 text-sm max-sm:justify-between max-sm:gap-2 max-sm:hidden">
-            <Link to='/' onClick={() => setMenu("inicio")} className={menu==="inicio" ? "pb-px border-solid border-b-2 border-slate-600 cursor-pointer" : "cursor-pointer"}>Inicio</Link>
-            <a href='#explore-restaurant'onClick={() => setMenu("menu")} className={menu==="menu" ? "pb-px border-solid border-b-2 border-slate-600 cursor-pointer" : "cursor-pointer"}> Menú</a>
-            <a href='#footer'onClick={() => setMenu("contact-us")} className={menu==="contact-us" ? "pb-px border-solid border-b-2 border-slate-600 cursor-pointer" : "cursor-pointer"}> Contact Us</a>
-
-        </ul>
-        <div className="flex items-center gap-10 max-sm:gap-3">
-            <img src={assets.search_icon} alt="" className="w-6 max-sm:w-4"/>
-            <div className="relative">
-               <Link  to='/cart'> <img src={assets.basket_icon} alt="" className="w-6 max-sm:w-4"/> </Link> 
-                <div className={getTotalCartAmount()===0?"":"absolute min-w-3 min-h-3 bg-orange-400 rounded-3xl bottom-6 left-4 max-sm:min-h-2 max-sm:min-w-2 max-sm:bottom-3"}></div>
-            </div>
-            {!token ?  
-            <button onClick={()=>setShowLogin(true)} className=" bg-transparent text-sm text-slate-600 border-2 border-orange-600 py-2 px-2  rounded-3xl cursor-pointer transition-transform hover:bg-red-50 max-sm:text-xs max-sm:w-20 max-sm:py-1 max-sm:px-2">Inicia Sesión</button>
-            : <div className="">
-              <img src={assets.profile_icon} alt=""/>
-              <ul className="">
-                 <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt=""/><p>Orders</p> </li>
-                 <hr/>
-                 <li onClick={logout} ><img src={assets.logout_icon} alt="" /><p>Logout</p> </li>
-                 
-              </ul>
-            </div>
+      <Link to="/">
+        {" "}
+        <img src={assets.logo} alt="" className="w-32 max-sm:w-16" />{" "}
+      </Link>
+      <ul className="flex gap-5 list-none text-slate-600 text-sm max-sm:justify-between max-sm:gap-2 max-sm:hidden">
+        <Link
+          to="/"
+          onClick={() => setMenu("inicio")}
+          className={
+            menu === "inicio"
+              ? "pb-px border-solid border-b-2 border-slate-600 cursor-pointer"
+              : "cursor-pointer"
           }
-           
+        >
+          Inicio
+        </Link>
+        <a
+          href="#explore-restaurant"
+          onClick={() => setMenu("menu")}
+          className={
+            menu === "menu"
+              ? "pb-px border-solid border-b-2 border-slate-600 cursor-pointer"
+              : "cursor-pointer"
+          }
+        >
+          {" "}
+          Menú
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={
+            menu === "contact-us"
+              ? "pb-px border-solid border-b-2 border-slate-600 cursor-pointer"
+              : "cursor-pointer"
+          }
+        >
+          {" "}
+          Contact Us
+        </a>
+      </ul>
+      <div className="flex items-center gap-10 max-sm:gap-3">
+        <img src={assets.search_icon} alt="" className="w-6 max-sm:w-4" />
+        <div className="relative">
+          <Link to="/cart">
+            {" "}
+            <img
+              src={assets.basket_icon}
+              alt=""
+              className="w-6 max-sm:w-4"
+            />{" "}
+          </Link>
+          <div
+            className={
+              getTotalCartAmount() === 0
+                ? ""
+                : "absolute min-w-3 min-h-3 bg-orange-400 rounded-3xl bottom-6 left-4 max-sm:min-h-2 max-sm:min-w-2 max-sm:bottom-3"
+            }
+          ></div>
         </div>
+        {!token ? (
+          <button
+            onClick={() => setShowLogin(true)}
+            className=" bg-transparent text-sm text-slate-600 border-2 border-orange-600 py-2 px-2  rounded-3xl cursor-pointer transition-transform hover:bg-red-50 max-sm:text-xs max-sm:w-20 max-sm:py-1 max-sm:px-2"
+          >
+            Inicia Sesión
+          </button>
+        ) : (
+          <div className="relative group">
+            <img src={assets.profile_icon} alt="" className=" w-5 " />
+            <ul className="absolute hidden  right-0 z-10 group-hover:flex group-hover:flex-col bg-slate-200 pr-8 pl-3 py-4 border-2 border-orange-500 rounded-md outline outline-2 outline-white list-none  ">
+              <li
+                onClick={() => navigate("/myorders")}
+                className="flex items-center gap-2.5 cursor-pointer hover:text-orange-500"
+              >
+                <img src={assets.bag_icon} alt="" className=" w-5 " />
+                <p>My Orders</p>
+              </li>
+              <hr className="border-black" />
+              <li
+                onClick={logout}
+                className="flex items-center gap-2.5 cursor-pointer hover:text-orange-500"
+              >
+                <img src={assets.logout_icon} alt="" className=" w-5 " />
+                <p>Logout</p>{" "}
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-    
+    </div>
   );
 };
 
